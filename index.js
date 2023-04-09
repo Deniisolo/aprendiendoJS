e41f44e05bae5e0a1a5a74b48fc416bd76c98c36
Denisolo <deniisolo@outlook.es>
2023-04-09 14:54:31 -0500

    realicé un Nav-bar, puse estilo y algunos comandos de js

---
+++ index.js
@@ -0,0 +1,11 @@
+//document.getElementById("selección").style.backgroundColor="red";
+//document.getElementById("selección").style.fontSize="24px"
+console.log("hola mundo");
+/*hola soy Denis hablando XOXOX 
+este comentario es para varia lineas
+*/
+// este comentario es SOLO para una linea
+
+var mivariable = "hola mundo"
+
+document.getElementById("mielemento").innerHTML = mivariable;
